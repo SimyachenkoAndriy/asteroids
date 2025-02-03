91d75618b8b4b9954cba4634f384002d5d7e0903
Andriy Simyachenko <simyachenko.andriy@gmail.com>
2025-02-03 17:30:21 +0200

    player model is drawing already

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
-from constants import *
 import pygame
+from constants import *
+from player import Player
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
   
-        screen.fill('red')
+        screen.fill('black')
+        player.draw(screen)
+        clock.tick(60)   # 60 frames per second
+        dt = clock.tick(60) / 1000
 
-                      
-  
-
-
-
-
-    
-    display.flip()
-
-
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
     main()
-
